356ca4b036258322d03b75699b017730568ddc9f
ceellyy9711 <43806765+ceellyy9711@users.noreply.github.com>
2022-03-25 10:29:24 +0900

    주석 정리

--- lib/dto/item_dto.dart
+++ lib/dto/item_dto.dart
@@ -11,8 +11,6 @@ class ItemDTO {
       {this.sdate, this.stationcode, this.itemcode, this.timecode, this.value});
 
   factory ItemDTO.fromJson(Map<String, dynamic> json) {
-    // DateTime dateTime = DateTime.parse(json['SDATE']);
-    // String dateTimeString = DateFormat.yMd('ko_KR').format(dateTime);
     return ItemDTO(
       sdate: json['SDATE'],
       stationcode: json['STATIONCODE'],
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
-import 'package:dust_app/dust_bloc.dart';
 import 'package:intl/intl.dart';
 import 'dto/dust_dto.dart';
 
@@ -8,7 +7,6 @@ import 'package:flutter/material.dart';
 
 import 'dto/item_dto.dart';
 import 'dto/metal_service_dto.dart';
-import 'dust_api_repository.dart';
 
 void main() {
   runApp(const DustApp());
@@ -23,10 +21,8 @@ class DustApp extends StatefulWidget {
 
 class _DustAppState extends State<DustApp> {
   Future<MetalServiceDTO>? _dustDTO;
-  // DustBloc? dustBloc;
-  // int? totalCount = _dustDTO?.metalService?.totalCount;
 
-  Future<MetalServiceDTO> getData() async{
+  Future<MetalServiceDTO> getData() async {
     var now = DateTime.now();
     var dateFormat = 'yyyyMMdd';
     // String serviceKey = 'k%2FnvUukcVq9w7HLnHS03TEXra7Jomzfb%2BLjl3tdirxE887d9CKnYeLL4VXIvyF6m1LFZl%2FSjcGElC955tRUuDQ%3D%3D';
@@ -39,8 +35,10 @@ class _DustAppState extends State<DustApp> {
     final response = await http.get(url);
     print('Response status: ${response.statusCode}');
     print('Response body: ${response.body}');
+
     DustDTO dustDTO = DustDTO.fromJson(json.decode(response.body));
-    MetalServiceDTO serviceDTO = dustDTO.metalService!;
+    MetalServiceDTO serviceDTO =
+        dustDTO.metalService!; //DustDTO 안의 MetalServiceDTO에 접근하기 위해
     return serviceDTO;
   }
 
@@ -52,116 +50,141 @@ class _DustAppState extends State<DustApp> {
 
   @override
   Widget build(BuildContext context) {
-    // int? length = _dustDTO?.metalService?.item?.length;
     return MaterialApp(
-      title: '미세먼지',
-      home: Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.white,
-          title: const Text('미세먼지', style: TextStyle(color: Colors.black),),
-        ),
-        body: Center(
-                child: FutureBuilder<MetalServiceDTO>(
-                  future: _dustDTO,
-                  builder: (context, snapshot){
-                    ItemDTO dto= ItemDTO();
-                    int temp = 0;
-                    if(snapshot.hasData){
-                      for (var element in snapshot.data!.item!) {
-                        if(temp==0){
-                          // dto.sdate = element.sdate;
-                          // dto.stationcode = element.stationcode;
-                          // dto.itemcode = element.itemcode;
-                          // dto.timecode = element.timecode;
-                          // dto.value = element.value;
-                          dto = ItemDTO(sdate: element.sdate, stationcode: element.stationcode, itemcode: element.itemcode, timecode: element.timecode, value: element.value);
+        title: '미세먼지',
+        home: Scaffold(
+          appBar: AppBar(
+            backgroundColor: Colors.white,
+            title: const Text(
+              '미세먼지',
+              style: TextStyle(color: Colors.black),
+            ),
+          ),
+          body: Center(
+            child: FutureBuilder<MetalServiceDTO>(
+                future: _dustDTO,
+                builder: (context, snapshot) {
+                  ItemDTO dto = ItemDTO(); //현재 비어있음
+                  int temp = 0;
+                  if (snapshot.hasData) {
+                    for (var element in snapshot.data!.item!) {
+                      if (temp == 0) {
+                        dto = ItemDTO(
+                            sdate: element.sdate,
+                            stationcode: element.stationcode,
+                            itemcode: element.itemcode,
+                            timecode: element.timecode,
+                            value: element.value);
+                        temp++;
+                      } else {
+                        if (num.parse(dto.sdate!) < num.parse(element.sdate!)) {  //date가 가장 큰 값 찾는 과정
+                          dto = ItemDTO(
+                              sdate: element.sdate,
+                              stationcode: element.stationcode,
+                              itemcode: element.itemcode,
+                              timecode: element.timecode,
+                              value: element.value);
                           temp++;
-                        } else {
-                          if(num.parse(dto.sdate!) < num.parse(element.sdate!)){
-                            // dto.sdate = element.sdate;
-                            // dto.stationcode = element.stationcode;
-                            // dto.itemcode = element.itemcode;
-                            // dto.timecode = element.timecode;
-                            // dto.value = element.value;
-                           dto = ItemDTO(sdate: element.sdate, stationcode: element.stationcode, itemcode: element.itemcode, timecode: element.timecode, value: element.value);
-                            temp++;
-                          }
                         }
                       }
-                      String text(ItemDTO dto){
-                        if(dto.value! < 5){
-                          return '아주 좋음';
-                        } else if(dto.value! < 10){
-                          return '좋음';
-                        } else if(dto.value! <15){
-                          return '보통';
-                        } else if(dto.value! <20){
-                          return '나쁨';
-                        } else if(dto.value! <30){
-                          return '매우 나쁨';
-                        } else return '최악';
-                      }
-                      String icon(ItemDTO dto){
-                        if(dto.value! < 5){
-                          return 'assets/happy.png';
-                        } else if(dto.value! < 10){
-                          return 'assets/smile.png';
-                        } else if(dto.value! <15){
-                          return 'assets/normal.png';
-                        } else if(dto.value! <20){
-                          return 'assets/bad.png';
-                        } else if(dto.value! <30){
-                          return 'assets/very_bad.png';
-                        } else return 'assets/worst.png';
-                      }
-                      Color backgroundColor(ItemDTO dto){
-                        if(dto.value! < 5){
-                          return Colors.tealAccent;
-                        } else if(dto.value! < 10){
-                          return Colors.lightGreenAccent;
-                        } else if(dto.value! <15){
-                          return Colors.white70;
-                        } else if(dto.value! <20){
-                          return Colors.yellowAccent;
-                        } else if(dto.value! <30){
-                          return Colors.orangeAccent;
-                        } else return Colors.redAccent;
-                      }
-
-                      var now = DateTime.now();
-                      var dateFormat = 'yyyy년 MM월 dd일';
-                      var timeFormat = 'HH시 mm분';
-
-                      return Container(
-                        width: double.infinity,
-                        color: backgroundColor(dto),
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.start,
-                          crossAxisAlignment: CrossAxisAlignment.center,
-                          children: [
-                            const Padding(padding: EdgeInsets.all(50)),
-                            Text(DateFormat(dateFormat).format(now), style: const TextStyle(fontSize: 20),),
-                            const SizedBox(height: 10,),
-                            Text(DateFormat(timeFormat).format(now), style: const TextStyle(fontSize: 20),),
-                            const SizedBox(height: 20,),
-                            Text('${dto.value?.floor()}',
-                              style: const TextStyle(fontSize: 50, fontWeight: FontWeight.bold), ),
-                            const SizedBox(height: 20,),
-                            // const Image(image: AssetImage('assets/happy.png')),
-                            Image.asset(icon(dto), height: 100),
-                            const SizedBox(height: 20,),
-                            Text(text(dto), style: const TextStyle(fontSize: 40),)
-                          ],
-                        ),
-                      );
-                    } else if(snapshot.hasError){
-                      return Text('${snapshot.error}');
                     }
-                    return const CircularProgressIndicator();
+                    String text(ItemDTO dto) {
+                      if (dto.value! < 5) {
+                        return '아주 좋음';
+                      } else if (dto.value! < 10) {
+                        return '좋음';
+                      } else if (dto.value! < 15) {
+                        return '보통';
+                      } else if (dto.value! < 20) {
+                        return '나쁨';
+                      } else if (dto.value! < 30) {
+                        return '매우 나쁨';
+                      } else
+                        return '최악';
+                    }
+
+                    String icon(ItemDTO dto) {
+                      if (dto.value! < 5) {
+                        return 'assets/happy.png';
+                      } else if (dto.value! < 10) {
+                        return 'assets/smile.png';
+                      } else if (dto.value! < 15) {
+                        return 'assets/normal.png';
+                      } else if (dto.value! < 20) {
+                        return 'assets/bad.png';
+                      } else if (dto.value! < 30) {
+                        return 'assets/very_bad.png';
+                      } else
+                        return 'assets/worst.png';
+                    }
+
+                    Color backgroundColor(ItemDTO dto) {
+                      if (dto.value! < 5) {
+                        return Colors.tealAccent;
+                      } else if (dto.value! < 10) {
+                        return Colors.lightGreenAccent;
+                      } else if (dto.value! < 15) {
+                        return Colors.white70;
+                      } else if (dto.value! < 20) {
+                        return Colors.yellowAccent;
+                      } else if (dto.value! < 30) {
+                        return Colors.orangeAccent;
+                      } else
+                        return Colors.redAccent;
+                    }
+
+                    var now = DateTime.now();
+                    var dateFormat = 'yyyy년 MM월 dd일';
+                    var timeFormat = 'HH시 mm분';
+
+                    return Container(
+                      width: double.infinity,
+                      color: backgroundColor(dto),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          const Padding(padding: EdgeInsets.all(50)),
+                          Text(
+                            DateFormat(dateFormat).format(now),   //날짜
+                            style: const TextStyle(fontSize: 20),
+                          ),
+                          const SizedBox(
+                            height: 10,
+                          ),
+                          Text(
+                            DateFormat(timeFormat).format(now),   //시간
+                            style: const TextStyle(fontSize: 20),
+                          ),
+                          const SizedBox(
+                            height: 20,
+                          ),
+                          Text(
+                            '${dto.value?.floor()}',    //미세먼지 수치
+                            style: const TextStyle(
+                                fontSize: 50, fontWeight: FontWeight.bold),
+                          ),
+                          const SizedBox(
+                            height: 20,
+                          ),
+                          // const Image(image: AssetImage('assets/happy.png')),
+                          Image.asset(icon(dto), height: 100),
+                          const SizedBox(
+                            height: 20,
+                          ),
+                          Text(
+                            text(dto),
+                            style: const TextStyle(fontSize: 40),
+                          )
+                        ],
+                      ),
+                    );
+                  } else if (snapshot.hasError) {
+                    return Text('${snapshot.error}');
                   }
-                ),
-              ),
-      )
-    );
+                  return const CircularProgressIndicator();
+                }),
+          ),
+        ));
   }
 }
